6b6a409944c1d9255a4dd9e97d46034225ffb220
Dr. Nandor Tamaskovics <numericalfreedom@googlemail.com>
2020-03-02 00:01:43 +0100

    Doxyfile and python files added.

---
+++ pytochnog/pytochnog.py
@@ -0,0 +1,32 @@
+
+
+class Pytochnog:
+     """
+     Class Pytochnog
+     """
+
+    def __init__( self ):
+        """
+        Constructor
+        """
+
+        self.x = 0 ;
+        """
+        Member variable x
+        """
+
+        self.y = 0 ;
+        """
+        Member variable x
+        """
+
+        pass
+
+
+    def echo( self , status ):
+        """
+        Function echo
+        """
+
+        pass
+
