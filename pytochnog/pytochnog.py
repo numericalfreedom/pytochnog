51cb095aaa767f3aa19e867b8b7fcf42b688e7a4
Numericalfreedom <numericalfreedom@googlemail.com>
2020-03-02 17:48:38 +0100

    pytochnog updated.

--- pytochnog/pytochnog.py
+++ pytochnog/pytochnog.py
@@ -1,32 +1,30 @@
-
-
+## Class Pytochnog
+#
+#
 class Pytochnog:
-     """
-     Class Pytochnog
-     """
 
+    ## Python object constructor
+    #
+    #
     def __init__( self ):
-        """
-        Constructor
-        """
 
-        self.x = 0 ;
-        """
-        Member variable x
-        """
+        ## Member variable realvalue
+        #
+        #  This is member variable realvalue holding a real value
+        #
+        self.realvalue = 0 ;
 
-        self.y = 0 ;
-        """
-        Member variable x
-        """
-
+        ## Member variable integervalue
+        #
+        #  This is member variable integervalue holding an integer value
+        #
+        self.integervalue = 0 ;
         pass
 
-
+    ## Function echo: implements the echo command
+    #
+    #  @param self   : the object instance
+    #  @param status : the echo status
+    #
     def echo( self , status ):
-        """
-        Function echo
-        """
-
         pass
-
