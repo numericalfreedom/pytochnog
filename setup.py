c2d56ee99ad236e45bde48bf8bbeee04517e687c
Numerical Freedom <numericalfreedom@googlemail.com>
2020-03-03 13:04:08 +0100

    project renamed to pytochnog3.

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytochnog",
+    name="pytochnog3",
     version="0.0.1",
     author="Numerical Freedom Foundation",
     author_email="numericalfreedom@googlemail.com",
     description="A python tool to the Tochnog Professional geotechnical FEM analysis programme",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/numericalfreedom/pytochnog",
+    url="https://github.com/numericalfreedom/pytochnog3",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
 
